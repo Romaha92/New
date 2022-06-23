d95a7b4add920c6f2253b991b58ad0c262decbe4
Roman <semenenko_garena@i.ua>
2022-06-23 13:55:16 +0200

    first commit

---
+++ JSpoject/js/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+
+
+
+ let number = 4;
+ console.log (4/2);
+
+ const persone = 'Alex';
+
+ // Числа пишуться без ковычек, имена и данные в ковычках//
+
+
+
+const arr = [1, 2, 3];
+console.log(arr[1]);
+
+
+const obj = {
+    "Anna": 500,
+    "Alise": 300,
+    "Angela":200
+};
+
+console.log(obj);
+
+const category = 'toys';
+
+console.log(`https://somelur.com/${category}/5`);
+
+const user = 'Roman';
+
+alert(`Hej, ${user}`);
